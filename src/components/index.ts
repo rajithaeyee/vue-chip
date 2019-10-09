04b0b9cc38a15f6be927b4656e994695cb4fdf11
rajithaeyee <rajithaeye@gmail.com>
2019-10-10 01:50:29 +0530

    tobe used as vue.use

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,17 @@
-import VChip from "@/components/VChip.vue"
-export default VChip;
+import  vue  from 'vue';
+import VChip from '@/components/VChip.vue'
+
+declare module 'vue/types/vue' {
+    export interface Vue {
+      VueChip: VChip;
+    }
+  }
+
+const VueChip = {
+    install: function (Vue : typeof vue, options:any) {
+        Vue.component('vue-chip', VChip);
+    },
+}
+
+export { VueChip };
+export default VueChip;
